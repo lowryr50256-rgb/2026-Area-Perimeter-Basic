4e348ea4dbdbf0c6fdd8c4dec422d7541b33045a
lowryr50256-rgb <lowryr50256@masseyhigh.school.nz>
2026-06-18 13:27:10 +1200

    Varibale Basics

---
+++ P_02_varriables.py
@@ -0,0 +1,14 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# constants are uppercase
+PI = 3.15
+
+a_01_my_num = 7
+
